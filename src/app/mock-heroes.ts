4b82aa6b15741917e7e9bd5eab688fbbd4f84cf0
Usama Chaudary <usamaachaudary@gmail.com>
2021-03-02 20:14:32 -0900

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = 
+[
+  { id: 11, name: 'Adil' },
+  { id: 12, name: 'Uzair' },
+  { id: 13, name: 'Mansoor' },
+  { id: 14, name: 'Hamza' },
+  { id: 15, name: 'Asim' },
+  { id: 16, name: 'Talha' },
+  { id: 17, name: 'Umair' },
+  { id: 18, name: 'Dr Muazam' },
+  { id: 19, name: 'Rashid' },
+  { id: 20, name: 'Rehan Tariq' }
+];
