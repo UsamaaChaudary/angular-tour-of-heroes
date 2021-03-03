59db152ad58423e406d3973d2a3cf31bf5553182
Usama Chaudary <usamaachaudary@gmail.com>
2021-03-03 12:30:17 -0900

    Added Additional Messages Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 // import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -14,6 +15,7 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void 
   {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   heroes: Hero[] = [];
@@ -24,7 +26,7 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void 
   {
